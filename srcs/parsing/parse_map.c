b717794edb585c39925287dfa4486eab6ad40376
Axel Brisse <axel.brisse@gmail.com>
2023-04-03 23:50:03 +0200

    j'ai fait de la merde

---
+++ srcs/parsing/parse_map.c
@@ -0,0 +1,27 @@
+#include "cub3d.h"
+
+// static bool	init_map(t_map *map, char **lines)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (lines[i] != NULL)
+// 	{
+// 		++i;
+// 	}
+// }
+
+bool	parse_map(t_map *map, char **lines)
+{
+	// int		i;
+	// int		len;
+
+	// i = 0;
+	// while (lines[i] != NULL)
+	// {
+	// 	++i;
+	// }
+	(void)map;
+	(void)lines;
+	return (false);
+}
