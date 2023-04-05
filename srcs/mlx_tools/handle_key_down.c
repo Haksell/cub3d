2de623352f7c6ddf5b2e7c44767bf8c536dc2621
Axel Brisse <axel.brisse@gmail.com>
2023-04-05 18:27:16 +0200

    rotate camera

--- srcs/mlx_tools/handle_key_down.c
+++ srcs/mlx_tools/handle_key_down.c
@@ -4,6 +4,7 @@ int	handle_key_down(int keycode, t_data *data)
 {
 	const t_vec2	scaled_dir = vec2_scale(data->player.dir, SPEED);
 	t_vec2			new_pos;
+	double			turn;
 
 	if (keycode == XK_Escape)
 		close_window(data);
@@ -20,9 +21,11 @@ int	handle_key_down(int keycode, t_data *data)
 	else if (keycode == XK_Left || keycode == XK_Right)
 	{
 		if (keycode == XK_Left)
-			data->player.dir = vec2_rotate(data->player.dir, TURN);
+			turn = TURN;
 		else
-			data->player.dir = vec2_rotate(data->player.dir, -TURN);
+			turn = -TURN;
+		data->player.dir = vec2_rotate(data->player.dir, turn);
+		data->camera = vec2_rotate(data->camera, turn);
 	}
 	return (EXIT_SUCCESS);
 }
