e98ae9dda67b535d4ee57c031707af30cfe2df31
Axel Brisse <axel.brisse@gmail.com>
2023-04-06 03:08:50 +0200

    render_frame is normified

--- srcs/display/render_frame.c
+++ srcs/display/render_frame.c
@@ -1,29 +1,5 @@
 #include "cub3d.h"
 
-static void	set_step_and_side_dist(t_dda *dda, t_vec2 *pos)
-{
-	if (dda->ray_dir.x < 0)
-	{
-		dda->step_x = -1;
-		dda->side_dist.x = (pos->x - dda->map_x) * dda->delta_dist.x;
-	}
-	else
-	{
-		dda->step_x = 1;
-		dda->side_dist.x = (dda->map_x + 1 - pos->x) * dda->delta_dist.x;
-	}
-	if (dda->ray_dir.y < 0)
-	{
-		dda->step_y = -1;
-		dda->side_dist.y = (pos->y - dda->map_y) * dda->delta_dist.y;
-	}
-	else
-	{
-		dda->step_y = 1;
-		dda->side_dist.y = (dda->map_y + 1 - pos->y) * dda->delta_dist.y;
-	}
-}
-
 static void	init_dda(t_data *data, t_dda *dda, int x)
 {
 	dda->camera_x = -(2 * x / ((double)WINDOW_WIDTH - 1) - 1);
@@ -33,7 +9,18 @@ static void	init_dda(t_data *data, t_dda *dda, int x)
 	dda->map_y = (int)data->player.pos.y;
 	dda->delta_dist.x = fabs(1.0 / dda->ray_dir.x);
 	dda->delta_dist.y = fabs(1.0 / dda->ray_dir.y);
-	set_step_and_side_dist(dda, &data->player.pos);
+	dda->step_x = (dda->ray_dir.x >= 0) - (dda->ray_dir.x < 0);
+	dda->step_y = (dda->ray_dir.y >= 0) - (dda->ray_dir.y < 0);
+	if (dda->ray_dir.x < 0)
+		dda->side_dist.x = data->player.pos.x - dda->map_x;
+	else
+		dda->side_dist.x = dda->map_x + 1 - data->player.pos.x;
+	if (dda->ray_dir.y < 0)
+		dda->side_dist.y = data->player.pos.y - dda->map_y;
+	else
+		dda->side_dist.y = dda->map_y + 1 - data->player.pos.y;
+	dda->side_dist.x *= dda->delta_dist.x;
+	dda->side_dist.y *= dda->delta_dist.y;
 }
 
 static void	execute_dda(t_data *data, t_dda *dda)
@@ -101,24 +88,18 @@ static void	terminate_dda(t_data *data, t_dda *dda)
 		dda->tex_x = dda->texture.width - dda->tex_x - 1;
 }
 
-static void	render_column(t_data *data, int x)
-{
-	t_dda	dda;
-
-	init_dda(data, &dda, x);
-	execute_dda(data, &dda);
-	terminate_dda(data, &dda);
-	draw_column(data, x, &dda);
-}
-
 int	render_frame(t_data *data)
 {
-	int	x;
+	t_dda	dda;
+	int		x;
 
 	x = 0;
 	while (x < WINDOW_WIDTH)
 	{
-		render_column(data, x);
+		init_dda(data, &dda, x);
+		execute_dda(data, &dda);
+		terminate_dda(data, &dda);
+		draw_column(data, x, &dda);
 		++x;
 	}
 	++data->frame;
