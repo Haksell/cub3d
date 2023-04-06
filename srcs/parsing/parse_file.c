7a735d535b28deecc1fa002b7be071c493e0908b
Axel Brisse <axel.brisse@gmail.com>
2023-04-06 05:53:45 +0200

    no more leaks on valid maps

--- srcs/parsing/parse_file.c
+++ srcs/parsing/parse_file.c
@@ -17,17 +17,19 @@ static bool	is_all_space(char *line)
 bool	parse_file(t_data *data, int argc, char **argv)
 {
 	char	**lines;
+	char	**lines_to_free;
 
 	if (argc != 2 || !ft_endswith(argv[1], ".cub"))
 		return (complain_bool(USAGE_MANDATORY));
 	lines = get_lines(argv[1]);
 	if (lines == NULL)
 		return (false);
+	lines_to_free = lines;
 	if (!parse_infos(&data->infos, &lines))
 		return (false);
 	while (*lines != NULL && is_all_space(*lines))
 		++lines;
 	if (!parse_map(data, lines))
-		return (false);
-	return (true);
+		return (free(lines_to_free[0]), free(lines_to_free), false);
+	return (free(lines_to_free[0]), free(lines_to_free), true);
 }
